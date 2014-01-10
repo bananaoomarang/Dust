3125ac0ab0f0c1ae5c51f243f1fc7d1ef2ca4162
Bananaoomarang <milomord@gmail.com>
2014-01-10 17:42:24 +0000

    Added probably broken gl

--- client/Dust.js
+++ client/Dust.js
@@ -10,9 +10,9 @@ function Dust() {
     var self = this;
 
     this.socket = io.connect('http://192.168.1.77:9966');
-    this.width  = $('#canvainer').width(),
-    this.height = $('#canvainer').height(),
-    this.renderer = this.createRenderer();
+    this.width  = $('#canvainer').width();
+    this.height = $('#canvainer').height();
+    this.gl = this.getGL();
     this.world = this.initWorld();
     this.selectionBox = null;
 
@@ -21,30 +21,17 @@ function Dust() {
         window.particleArray[i] = [];
         for (var j = 0; j <= this.height; j++) {
             window.particleArray[i][j] = 0;
-        };
-    };
-    
+        }
+    }
+}
 
-    $(window).resize(function() {
-        self.width = $('#canvainer').width();
-        self.height = $('#canvainer').height();
-        
-        self.resizeRenderer(self.width, self.height);
-    });
-};
-
-Dust.prototype.createRenderer = function() {
+Dust.prototype.getGL = function() {
     htmlCanvas = "<canvas width=" + "\"" + this.width + "\"" + "height=" + "\"" + this.height + "\"" + "></canvas>";
 
     $('#canvainer').append(htmlCanvas);
 
-    return $('canvas').get(0).getContext('2d');
-}
-
-Dust.prototype.resizeRenderer = function(w, h) {
-    this.renderer.width = w;
-    this.renderer.height = h;
-}
+    return $('canvas').get(0).getContext('webgl');
+};
 
 Dust.prototype.initWorld = function() {
     // Define world
@@ -60,72 +47,44 @@ Dust.prototype.initWorld = function() {
     world.pushSolid(solid);
 
     return world;
-}
+};
 
 Dust.prototype.updateWorld = function(dt) {
     this.world.update(dt);
-}
+};
 
 Dust.prototype.drawWorld = function() {
     var self = this;
-
-    this.renderer.clearRect(0, 0, this.width, this.height);
-
-    this.renderer.fillStyle = 'black';
-
-    // Draw solids
-    for (var i = 0; i < this.world.solids.length; i++) {
-        var s = this.world.solids[i];
-
-        this.renderer.fillRect(s.pos.x, s.pos.y, s.w, s.h);
-    };
-
-    this.renderer.fillStyle = 'yellow';
-
-    // Draw sand
-    for (var i = 0; i < this.world.sands.length; i++) {
-        var s = this.world.sands[i];
-        if(s.x === 0) console.log('fdfa');
-        this.renderer.fillRect(s.x, s.y, 1, 1);
-    };
-
-    // Draw Selection Box (if one exists)
-    if(this.selectionBox !== null) {
-        this.renderer.beginPath();
-        this.renderer.strokeStyle = 'gray';
-        this.renderer.rect(this.selectionBox.pos.x, this.selectionBox.pos.y, this.selectionBox.w, this.selectionBox.h);
-        this.renderer.stroke();
-    }
-}
+};
 
 Dust.prototype.resizeSelection = function(w, h) {
     this.selectionBox.resize(w, h);
-}
+};
 
 Dust.prototype.moveSelection = function(vec) {
     this.selectionBox.move(vec);
-}
+};
 
 Dust.prototype.drawSelection = function(x, y, w, h) {
     this.selectionBox = new Solid(w, h, x, y);
-}
+};
 
 Dust.prototype.spawnSolid = function(x, y, w, h) {
     var s = new Solid(w, h, x, y);
     this.world.pushSolid(s);
-}
+};
 
 Dust.prototype.spawnDust = function(x, y) {
     var n = 50,
         area = 10;
     for (var i = 0; i < n; i++) {
-        var x = Math.round((x - area/2) + area*Math.random()),
-            y = Math.round((y - area/2) + area*Math.random()),
-            s = new Vector(x, y);
+        x = Math.round((x - area/2) + area*Math.random());
+        y = Math.round((y - area/2) + area*Math.random());
+        s = new Vector(x, y);
 
         if(!this.world.collides(s) && s.within(this.world.bounds)) this.world.pushSand(s);
-    };
-}
+    }
+};
 
 // If a solid exists here, it will be sandified
 Dust.prototype.sandifySolid = function(vec) {
@@ -135,9 +94,9 @@ Dust.prototype.sandifySolid = function(vec) {
             for (var y = 0; y < s.h; y++) {
                 var sand = new Vector(x + s.pos.x, y + s.pos.y);
                 this.world.pushSand(sand);
-            };
-        };
+            }
+        }
         var index = this.world.solids.indexOf(s);
-        this.world.solids.splice(index, 1)
+        this.world.solids.splice(index, 1);
     }
-}
+};
